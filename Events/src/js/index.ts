35e6ca96b887bd17869436bd7b9bcb92cb1797f8
Vinícius Barbosa <vinibar95@gmail.com>
2023-10-27 07:53:57 -0300

    Atualização da sessão de eventos

--- Events/src/js/index.ts
+++ Events/src/js/index.ts
@@ -1,1 +1,25 @@
 
+const testEvent1 = document.querySelector('#testEvent1') as HTMLElement
+const testEvent2 = document.querySelector('#testEvent2')
+const testEvent3 = document.querySelector('#testEvent3')
+
+type EventFire = (this: HTMLElement, e: MouseEvent) => void
+
+const eventFire: EventFire = function(evt: MouseEvent){
+    console.log('event fired')
+    console.log(evt)
+    console.log(this)
+
+    const currentTarget = evt.currentTarget as HTMLElement
+
+    const quantity = this.querySelector('span') as HTMLSpanElement
+    //const n = parseInt(quantity.textContent || "0")
+}
+
+testEvent1.addEventListener("click", eventFire)
+
+
+
+//Disparando um evento
+const _event = new Event('click')
+testEvent2?.dispatchEvent(_event)
